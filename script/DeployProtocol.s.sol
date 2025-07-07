302c7c47d53ded0c0c8d18f2733d2801ea08b782
Kevin Kennis <kevinvkennis@gmail.com>
2025-07-07 08:45:19 -0700

    update deploy script

--- script/DeployProtocol.s.sol
+++ script/DeployProtocol.s.sol
@@ -14,8 +14,7 @@ import {Conduit} from "../src/conduit/Conduit.sol";
 
 contract DeployProtocolScript is Script {
     // Management addresses
-    address public constant MANAGER_1 = 0x0670faf0016E1bf591fEd8e0322689E894104F81;
-    address public constant MANAGER_2 = 0xc67DD6f32147285A9e4D92774055cE3Dba5Ae8b6;
+    address public constant MANAGER_1 = 0x91C64d8D530c494B65A7ae58B8534Ac5A05A3d43;
 
     // Storage for deployed contract addresses
     RoleManager public roleManager;
@@ -23,9 +22,7 @@ contract DeployProtocolScript is Script {
     Registry public registry;
     KycRulesHook public kycRulesHook;
     PriceOracleReporter public priceOracle;
-    ReportedStrategy public reportedStrategyImplementation;
-    // GatedMintReportedStrategy public gatedMintStrategyImplementation;
-    ManagedWithdrawReportedStrategy public managedWithdrawStrategyImplementation;
+    ManagedWithdrawReportedStrategy public strategyImplementation;
     Conduit public conduit;
 
     function setUp() public {}
@@ -55,14 +52,6 @@ contract DeployProtocolScript is Script {
 
         console.log("RoleManager deployed and roles configured.");
 
-        // Deploy mock USD token
-        usdToken = new MockERC20("Mock USD", "USDC", 6);
-
-        // Mint tokens to various addresses for testing
-        usdToken.mint(deployer, 50_000_000_000_000_000_000); // 50MM USDC with 6 decimals
-        usdToken.mint(MANAGER_1, 50_000_000_000_000_000_000); // 50MM USDC with 6 decimals
-        usdToken.mint(MANAGER_2, 50_000_000_000_000_000_000); // 50MM USDC with 6 decimals
-
         console.log("Mock USD Token deployed and minted to managers.");
 
         // Deploy Registry with role manager
@@ -74,8 +63,27 @@ contract DeployProtocolScript is Script {
         // Link registry to role manager
         roleManager.initializeRegistry(address(registry));
 
-        // Allow USD token as an asset
+        // Deploy mock USD token
+        usdToken = new MockERC20("Mock USD", "USDC", 6);
+
+        // Mint tokens to various addresses for testing
+        usdToken.mint(deployer, 50_000_000_000_000_000_000); // 50MM USDC with 6 decimals
+        usdToken.mint(MANAGER_1, 50_000_000_000_000_000_000); // 50MM USDC with 6 decimals
+        usdToken.mint(0x36Abd7ABde32E292707D81B225eA34a15CD92F6C, 50_000_000_000_000_000_000); // 50MM USDC with 6 decimals
+        usdToken.mint(0x75BbFf2206b6Ad50786Ee3ce8A81eDb72f3e381b, 50_000_000_000_000_000_000); // 50MM USDC with 6 decimals
+        usdToken.mint(0x76F2DAD4741CB0f4C8C56361d8cF5E05Bc01Bf28, 50_000_000_000_000_000_000); // 50MM USDC with 6 decimals
+
+        // Allow Mock USD token as an asset
         registry.setAsset(address(usdToken), 6);
+
+        address USDC_ON_BASE = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+        registry.setAsset(USDC_ON_BASE, 6); // Allow USDC on Base
+
+        address CBBTC = 0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf;
+        registry.setAsset(CBBTC, 8); // Allow CBBTC on Base
+
+        address WBTC = 0x0555E30da8f98308EdB960aa94C0Db47230d2B9c;
+        registry.setAsset(WBTC, 8); // Allow WBTC on Base
 
         // Deploy KYC Rules Hook with role manager
         kycRulesHook = new KycRulesHook(address(roleManager));
@@ -87,52 +95,37 @@ contract DeployProtocolScript is Script {
         // Allow addresses in KYC rules
         kycRulesHook.allow(deployer);
         kycRulesHook.allow(MANAGER_1);
-        kycRulesHook.allow(MANAGER_2);
+        kycRulesHook.allow(0x36Abd7ABde32E292707D81B225eA34a15CD92F6C);
         kycRulesHook.allow(0x75BbFf2206b6Ad50786Ee3ce8A81eDb72f3e381b);
-        kycRulesHook.allow(0x30C157C9749Df07f04e8170F5a529C89EF0a4639);
-        console.log("Managers allowed in KYC rules.");
+        kycRulesHook.allow(0x76F2DAD4741CB0f4C8C56361d8cF5E05Bc01Bf28);
+        console.log("Deployers allowed in KYC rules.");
 
         // Deploy Price Oracle Reporter with initial price of 1 USD
-        uint256 initialPrice = 1_000_000; // $1.00 with 6 decimals
+        uint256 initialPrice = 1e18; // $1.00 with 18 decimals
         priceOracle = new PriceOracleReporter(initialPrice, MANAGER_1, 100, 3600); // 1% max change per hour
-        priceOracle.setUpdater(MANAGER_2, true);
+        priceOracle.setUpdater(MANAGER_1, true);
         console.log("Price Oracle Reporter deployed.");
 
-        // Deploy ReportedStrategy implementation to be used as a template
-        reportedStrategyImplementation = new ReportedStrategy();
-        console.log("ReportedStrategy implementation deployed.");
-
-        // // Deploy GatedMintReportedStrategy implementation to be used as a template
-        // gatedMintStrategyImplementation = new GatedMintReportedStrategy();
-        // console.log("GatedMintReportedStrategy implementation deployed.");
-
         // Deploy ManagedWithdrawReportedStrategy implementation to be used as a template
-        managedWithdrawStrategyImplementation = new ManagedWithdrawReportedStrategy();
+        strategyImplementation = new ManagedWithdrawReportedStrategy();
         console.log("ManagedWithdrawReportedStrategy implementation deployed.");
 
         // Register both strategy implementations in the registry
-        registry.setStrategy(address(reportedStrategyImplementation), true);
-        // registry.setStrategy(address(gatedMintStrategyImplementation), true);
-        registry.setStrategy(address(managedWithdrawStrategyImplementation), true);
+        registry.setStrategy(address(strategyImplementation), true);
         console.log("Registry configured with strategy implementations.");
     }
 
     function grantRolesToManagers() internal {
         // Protocol admins
         roleManager.grantRole(MANAGER_1, roleManager.PROTOCOL_ADMIN());
-        roleManager.grantRole(MANAGER_2, roleManager.PROTOCOL_ADMIN());
 
         // Strategy roles
         roleManager.grantRole(MANAGER_1, roleManager.STRATEGY_ADMIN());
-        roleManager.grantRole(MANAGER_2, roleManager.STRATEGY_ADMIN());
         roleManager.grantRole(MANAGER_1, roleManager.STRATEGY_OPERATOR());
-        roleManager.grantRole(MANAGER_2, roleManager.STRATEGY_OPERATOR());
 
         // KYC roles
         roleManager.grantRole(MANAGER_1, roleManager.RULES_ADMIN());
-        roleManager.grantRole(MANAGER_2, roleManager.RULES_ADMIN());
         roleManager.grantRole(MANAGER_1, roleManager.KYC_OPERATOR());
-        roleManager.grantRole(MANAGER_2, roleManager.KYC_OPERATOR());
     }
 
     function logDeployedContracts() internal view {
@@ -144,8 +137,6 @@ contract DeployProtocolScript is Script {
         console.log("Conduit:", address(conduit));
         console.log("KYC Rules Hook:", address(kycRulesHook));
         console.log("Price Oracle Reporter:", address(priceOracle));
-        console.log("ReportedStrategy Implementation:", address(reportedStrategyImplementation));
-        // console.log("GatedMintReportedStrategy Implementation:", address(gatedMintStrategyImplementation));
-        console.log("ManagedWithdrawReportedStrategy Implementation:", address(managedWithdrawStrategyImplementation));
+        console.log("Strategy Implementation:", address(strategyImplementation));
     }
 }
